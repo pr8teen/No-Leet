1d56f5a25bde7bc92b25a084f8481a366612660b
Prasannajeet Ojha <110190225+pr8teen@users.noreply.github.com>
2025-07-31 14:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (3.67%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int cnt = 0;
-        for(int i=low;i<high+1;i++){
-            if(i%2!=0){
-                cnt+=1;
-            }
+        // This is the direct conversion of your Python logic.
+        // If the span between low and high is even, the number of odds and evens is the same.
+        // If the span is odd, one is more frequent. We check if the boundaries are odd.
+        int span = high - low;
+        if (low % 2 != 0 || high % 2 != 0) {
+            return span / 2 + 1;
+        } else {
+            return span / 2;
         }
-        return cnt;
     }
 };
