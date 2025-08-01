8c8a59bf74f136e57708db8d82ef0facfb5b0bf0
Prasannajeet Ojha <110190225+pr8teen@users.noreply.github.com>
2025-08-01 23:32:35 +0530

    Time: 379 ms (8.36%) | Memory: 17.5 MB (89.46%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -2,4 +2,5 @@ class Solution:
     def fib(self, n: int) -> int:
         if n==0 or n==1:
             return n
-        return self.fib(n-1) + self.fib(n-2)
+        dudh = self.fib(n-1) + self.fib(n-2)
+        return dudh
